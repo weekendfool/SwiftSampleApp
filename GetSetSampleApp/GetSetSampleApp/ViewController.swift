e769628b371bf50a462f9bd8f544c574e349f909
Ohara <nori.tukareta@gmail.com>
2020-10-01 16:44:43 +0900

    get setの挙動確認用ファイルとAppを生成

---
+++ GetSetSampleApp/GetSetSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetSetSampleApp
+//
+//  Created by 尾原徳泰 on 2020/10/01.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
