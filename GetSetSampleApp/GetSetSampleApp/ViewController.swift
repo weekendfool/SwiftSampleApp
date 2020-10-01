bc396d0b79f62c62242da24ecee89bc949a04289
Ohara <nori.tukareta@gmail.com>
2020-10-01 16:48:07 +0900

    getの起動を実装

--- GetSetSampleApp/GetSetSampleApp/ViewController.swift
+++ GetSetSampleApp/GetSetSampleApp/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Itemクラスのインスタンスを生成
+        var item = Item()
+        // Itemのgetを起動させる
+        item.intextPrice = 110
+        print(item.price)
     }
 
 
