19d86b09dd593460d08eb16901cf6d336a2c9ffe
Ohara <nori.tukareta@gmail.com>
2020-10-24 14:01:51 +0900

    画面構成完了

---
+++ FirebaseSignInSampleApp/FirebaseSignInSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseSignInSampleApp
+//
+//  Created by 尾原徳泰 on 2020/10/24.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirebaseSignInSampleApp/Pods/Target Support Files/Pods-FirebaseSignInSampleApp-FirebaseSignInSampleAppUITests/Pods-FirebaseSignInSampleApp-FirebaseSignInSampleAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebaseSignInSampleApp_FirebaseSignInSampleAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FirebaseSignInSampleApp_FirebaseSignInSampleAppUITests
+@end
