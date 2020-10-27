9079b5e56ce8c2c123aaeebeac3c9728163be2a3
Ohara <nori.tukareta@gmail.com>
2020-10-27 11:40:15 +0900

    SignupViewController実装

--- FirebaseSignInSampleApp/FirebaseSignInSampleApp/SignupViewController.swift
+++ FirebaseSignInSampleApp/FirebaseSignInSampleApp/SignupViewController.swift
@@ -35,18 +35,41 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
         
         // サインアップ処理
         // 第一引数にメールアドレス、第二引数にパスワード
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-          
-        }
-
+        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+            // エラーなしなら、認証完了
+            if error == nil {
+                // エラーがない場合はそのままログイン画面に飛び、ログイン
+                self.transitionToLogin()
+            } else {
+            print("\(error?.localizedDescription)")
+            }
+        })
     }
+
+    
     
     
 
     @IBAction func willSignup(_ sender: Any) {
+        signup()
     }
     
     @IBAction func willTransitionToLogin(_ sender: Any) {
+        transitionToLogin()
     }
     
+    // Login画面への遷移
+    func transitionToLogin() {
+        self.performSegue(withIdentifier: "toLogin", sender: self)
+    }
+    
+    // listViewControllerへの遷移
+    func transitionToView() {
+        self.performSegue(withIdentifier: "toView", sender: self)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
