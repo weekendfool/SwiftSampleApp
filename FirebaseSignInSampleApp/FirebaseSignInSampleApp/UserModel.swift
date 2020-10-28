827fb4826de725f5a2d8de14a7a5a3caebaa4048
Ohara <nori.tukareta@gmail.com>
2020-10-28 23:37:05 +0900

    UserModelファイル作成

---
+++ FirebaseSignInSampleApp/FirebaseSignInSampleApp/UserModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserModel.swift
+//  FirebaseSignInSampleApp
+//
+//  Created by 尾原徳泰 on 2020/10/28.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+struct User {
+    var name: String = ""
+    var age: Int = 0
+    var favoriteBook: String = ""
+}
