a07b63cf594069069282283abce3f5e79d0c5a3f
Ohara <nori.tukareta@gmail.com>
2020-10-31 00:30:15 +0900

    SetViewControllerファイルの実装

--- FireBaseGetSampleApp/FireBaseGetSampleApp/New Group/Controller/SetViewController.swift
+++ FireBaseGetSampleApp/FireBaseGetSampleApp/New Group/Controller/SetViewController.swift
@@ -7,20 +7,46 @@
 //
 
 import UIKit
+import Firebase
 
 class SetViewController: UIViewController {
 
+    // パーツの紐付け
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var ageTextField: UITextField!
     @IBOutlet weak var passTextField: UITextField!
     
+    //　Userのインスタンスを格納
+//    var user: User!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func addButtonAction(_ sender: Any) {
+        // 三つの項目が埋まっていないと送信できない処理
+        guard let nameText = nameTextField.text else { return }
+        guard let ageText = ageTextField.text else { return }
+        guard let passText = passTextField.text else { return }
+        
+        // firebaseのルートを宣言
+        let ref = Database.database().reference()
+            
+        // 入力内容をインスタンス内に格納する
+        var addUser = User()
+        addUser.name = nameText
+        addUser.age = Int(ageText)!
+        addUser.pass = passText
+        
+        // データを登録する
+        ref.child("Players").childByAutoId().setValue(addUser.toDictionary)
+        
+        // 登録後にテキストフィールドの初期化
+        nameTextField.text = ""
+        ageTextField.text = ""
+        passTextField.text = ""
+        
     }
     
     
--- FireBaseGetSampleApp/FireBaseGetSampleApp/New Group/Model/UserModel.swift
+++ FireBaseGetSampleApp/FireBaseGetSampleApp/New Group/Model/UserModel.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import Firebase
+
+struct User {
+    
+    // 構成する要素
+    var name: String = ""
+    var age: Int = 0
+    var pass: String = ""
+    
+    var toDictionary: [String: Any] {
+        return [
+            "name": name,
+            "age": age,
+            "pass": pass
+        ]
+        print(self.toDictionary)
+    }
+    
+    // firebaceからの返信をdictionaryで受け取る
+    // 受け取った情報を自身Userの要素に格納
+    mutating func setFromDictionary(_ dictionary: [String: Any]) {
+        // 引数を代入するか??以降を代入
+        name = dictionary["name"] as? String ?? ""
+        age = dictionary["age"] as? Int ?? 0
+        pass = dictionary["pass"] as? String ?? ""
+    }
+}
