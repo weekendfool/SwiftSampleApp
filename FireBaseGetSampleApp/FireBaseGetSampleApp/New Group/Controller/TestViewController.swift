d30ee5b48421fb4ea7684c55b19f5f07c4aaa148
Ohara <nori.tukareta@gmail.com>
2020-11-13 23:55:02 +0900

    新規サンプル作成

---
+++ FireBaseGetSampleApp/FireBaseGetSampleApp/New Group/Controller/TestViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TestViewController.swift
+//  FireBaseGetSampleApp
+//
+//  Created by 尾原徳泰 on 2020/11/13.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func testButtonAction(_ sender: Any) {
+    }
+    
+   
+
+}
---
+++ FirebaseGetObjectSampleApp/FirebaseGetObjectSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseGetObjectSampleApp
+//
+//  Created by 尾原徳泰 on 2020/11/13.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
