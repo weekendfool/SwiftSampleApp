b32b116c28898fe6881513a673c632fe12cf7370
Ohara <nori.tukareta@gmail.com>
2021-05-09 02:59:31 +0900

    Storyboard Referenceサンプル実装

---
+++ SampleLayoutApp/SampleLayoutApp/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  SampleLayoutApp
+//
+//  Created by 尾原徳泰 on 2021/05/09.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SampleLayoutApp/SampleLayoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleLayoutApp
+//
+//  Created by 尾原徳泰 on 2021/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
