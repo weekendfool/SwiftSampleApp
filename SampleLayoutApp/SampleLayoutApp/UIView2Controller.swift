8fa57edba980e4cb6e4756fbd3aa424712d0325e
Ohara <nori.tukareta@gmail.com>
2021-05-18 16:42:40 +0900

    StackView設定

---
+++ SampleLayoutApp/SampleLayoutApp/UIView2Controller.swift
@@ -0,0 +1,29 @@
+//
+//  UIView2Controller.swift
+//  SampleLayoutApp
+//
+//  Created by 尾原徳泰 on 2021/05/18.
+//
+
+import UIKit
+
+class UIView2Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
