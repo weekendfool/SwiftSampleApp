c02880f58ed482ff83b0f63c86c5ac3ee46e77ac
Ohara <nori.tukareta@gmail.com>
2021-05-30 16:20:09 +0900

    アプリ内の階層わけ

---
+++ RouterSampleApp/RouterSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RouterSampleApp
+//
+//  Created by 尾原徳泰 on 2021/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
