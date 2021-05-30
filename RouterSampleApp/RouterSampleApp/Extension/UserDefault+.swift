1dcfbd995de10e705976e25ec330e77cebd01358
Ohara <nori.tukareta@gmail.com>
2021-05-31 03:46:46 +0900

    UserDefault+実装

--- RouterSampleApp/RouterSampleApp/Extension/UserDefault+.swift
+++ RouterSampleApp/RouterSampleApp/Extension/UserDefault+.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+extension UserDefaults {
+    
+    private var loginedKey: String { "logined" }
+    
+    var isLogined: Bool {
+        set {
+            // 自身に値を返し格納する
+            self.setValue(newValue, forKey: loginedKey)
+        }
+        get {
+            // 自身にbool値を渡す
+            self.bool(forKey: loginedKey)
+        }
+    }
+}
--- RouterSampleApp/RouterSampleApp/Router/Router.swift
+++ RouterSampleApp/RouterSampleApp/Router/Router.swift
@@ -5,4 +5,21 @@
 //  Created by 尾原徳泰 on 2021/05/30.
 //
 
-import Foundation
+import UIKit
+
+// 画面遷移を管理
+final class Router {
+    
+    static let shared: Router = .init()
+    
+    private init() {}
+    
+    private (set) var window: UIWindow?
+    
+    // 起動直後の画面を表示する
+    func showRoot(window: UIWindow?) {
+        
+        // パラメータから初期画面を切り替える
+        if !UserDefaults.standard.isLogined
+    }
+}
