380b74ec200b2003bfa2845455a20061edb24e69
Ohara <nori.tukareta@gmail.com>
2021-06-02 18:28:51 +0900

    MVCCountryTableViewCell実装

--- MVCSampleApp/View/MVCCountryTableViewCell/MVCCountryTableViewCell.swift
+++ MVCSampleApp/View/MVCCountryTableViewCell/MVCCountryTableViewCell.swift
@@ -5,4 +5,26 @@
 //  Created by 尾原徳泰 on 2021/06/02.
 //
 
-import Foundation
+import UIKit
+
+final class MVCCountryTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var countryImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var moneyRationalLabel: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        countryImageView.image = nil
+        nameLabel.text = nil
+        moneyRationalLabel.text = nil
+    }
+    
+    func configure(country: MVCCountryModel) {
+        countryImageView.image = UIImage.init(named: country.imageName)
+        nameLabel.text = country.name
+        moneyRationalLabel.text = country.moneyRatio.description
+    }
+    
+}
+
