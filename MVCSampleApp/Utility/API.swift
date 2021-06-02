b4efeecd52624ec054dc819d1da2b91753433e45
Ohara <nori.tukareta@gmail.com>
2021-06-02 16:59:48 +0900

    API実装

--- MVCSampleApp/Utility/API.swift
+++ MVCSampleApp/Utility/API.swift
@@ -15,10 +15,135 @@ final class API {
     private init() {}
     
     // userデータを取得するAPI
-    func getUsers(completion: ([MVCUserModel]) -> Void)? = nil) {
+    func getUsers(completion: (([MVCUserModel]) -> Void)? = nil) {
     
-    // 仮想API通信
-    dis
+        // 仮想API通信
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4)  {
+            
+            guard let data = API.sampleData.data(using: .utf8),
+                  let users = try? JSONDecoder().decode([MVCUserModel].self, from: data)
+            else {
+                // data　OR usersがエラーのとき
+                completion?([])
+                return
+            }
+            completion?(users)
+        }
     
+    }
+    
+    //サーバーからこんな感じでデータが返ってくるとする
+    private static var sampleData: String {
+      let json =
+  """
+  [
+      {
+          "name": "tanaka",
+          "at_name": "@tanaka",
+          "switch": false
+      },
+      {
+          "name": "suzuki",
+          "at_name": "@suzuki",
+          "switch": true
+      },
+      {
+          "name": "yoshida",
+          "at_name": "@yoshida",
+          "switch": true
+      },
+      {
+          "name": "tanaka",
+          "at_name": "@tanaka",
+          "switch": false
+      },
+      {
+          "name": "suzuki",
+          "at_name": "@suzuki",
+          "switch": true
+      },
+      {
+          "name": "yoshida",
+          "at_name": "@yoshida",
+          "switch": true
+      },
+      {
+          "name": "tanaka",
+          "at_name": "@tanaka",
+          "switch": false
+      },
+      {
+          "name": "suzuki",
+          "at_name": "@suzuki",
+          "switch": true
+      },
+      {
+          "name": "yoshida",
+          "at_name": "@yoshida",
+          "switch": true
+      },
+      {
+          "name": "tanaka",
+          "at_name": "@tanaka",
+          "switch": false
+      },
+      {
+          "name": "suzuki",
+          "at_name": "@suzuki",
+          "switch": true
+      },
+      {
+          "name": "yoshida",
+          "at_name": "@yoshida",
+          "switch": true
+      },
+      {
+          "name": "tanaka",
+          "at_name": "@tanaka",
+          "switch": false
+      },
+      {
+          "name": "suzuki",
+          "at_name": "@suzuki",
+          "switch": true
+      },
+      {
+          "name": "yoshida",
+          "at_name": "@yoshida",
+          "switch": true
+      },
+      {
+          "name": "tanaka",
+          "at_name": "@tanaka",
+          "switch": false
+      },
+      {
+          "name": "suzuki",
+          "at_name": "@suzuki",
+          "switch": true
+      },
+      {
+          "name": "yoshida",
+          "at_name": "@yoshida",
+          "switch": true
+      },
+      {
+          "name": "tanaka",
+          "at_name": "@tanaka",
+          "switch": false
+      },
+      {
+          "name": "suzuki",
+          "at_name": "@suzuki",
+          "switch": true
+      },
+      {
+          "name": "yoshida",
+          "at_name": "@yoshida",
+          "switch": true
+      }
+  ]
+  """
+      return json
     }
 }
