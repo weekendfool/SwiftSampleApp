ac0dfcab0714cb002c1052b7bf65a60a0c645e77
Ohara <nori.tukareta@gmail.com>
2021-06-02 17:09:04 +0900

    MVCCountoryModel実装

--- MVCSampleApp/Model/MVCCountoryModel.swift
+++ MVCSampleApp/Model/MVCCountoryModel.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+enum MVCCountryModel: CaseIterable {
+    case america
+    case japan
+    case chaina
+    case france
+    
+    var name: String {
+        switch self {
+        case .america:
+            return "アメリカ"
+        case .japan:
+            return "日本"
+        case .chaina:
+            return "中国"
+        case .france:
+            return "フランス"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .america:
+            return "america"
+        case .japan:
+            return "japan"
+        case .chaina:
+            return "chaina"
+        case .france:
+            return "france"
+        }
+    }
+    
+    var moneyRatio: Double {
+        switch self {
+        case .america:
+            return 0.01
+        case .japan:
+            return 1
+        case .chaina:
+            return 0.5
+        case .france:
+            return 0.2
+        }
+    }
+}
+
