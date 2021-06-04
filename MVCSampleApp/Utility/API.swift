c1f5f31b955cd35a1a6ee81c733e729e4b54cf1a
Ohara <nori.tukareta@gmail.com>
2021-06-04 21:35:16 +0900

    API修正

--- MVCSampleApp/Utility/API.swift
+++ MVCSampleApp/Utility/API.swift
@@ -8,142 +8,144 @@
 import Foundation
 
 
+import Foundation
+
 final class API {
-    
-    // シングルトンパターンの書き方
-    static let shared = API()
-    private init() {}
-    
-    // userデータを取得するAPI
-    func getUsers(completion: (([MVCUserModel]) -> Void)? = nil) {
-    
-        // 仮想API通信
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4)  {
-            
-            guard let data = API.sampleData.data(using: .utf8),
-                  let users = try? JSONDecoder().decode([MVCUserModel].self, from: data)
-            else {
-                // data　OR usersがエラーのとき
-                completion?([])
-                return
-            }
-            completion?(users)
-        }
-    
-    }
-    
-    //サーバーからこんな感じでデータが返ってくるとする
-    private static var sampleData: String {
-      let json =
-  """
-  [
-      {
-          "name": "tanaka",
-          "at_name": "@tanaka",
-          "switch": false
-      },
-      {
-          "name": "suzuki",
-          "at_name": "@suzuki",
-          "switch": true
-      },
-      {
-          "name": "yoshida",
-          "at_name": "@yoshida",
-          "switch": true
-      },
-      {
-          "name": "tanaka",
-          "at_name": "@tanaka",
-          "switch": false
-      },
-      {
-          "name": "suzuki",
-          "at_name": "@suzuki",
-          "switch": true
-      },
-      {
-          "name": "yoshida",
-          "at_name": "@yoshida",
-          "switch": true
-      },
-      {
-          "name": "tanaka",
-          "at_name": "@tanaka",
-          "switch": false
-      },
-      {
-          "name": "suzuki",
-          "at_name": "@suzuki",
-          "switch": true
-      },
-      {
-          "name": "yoshida",
-          "at_name": "@yoshida",
-          "switch": true
-      },
-      {
-          "name": "tanaka",
-          "at_name": "@tanaka",
-          "switch": false
-      },
-      {
-          "name": "suzuki",
-          "at_name": "@suzuki",
-          "switch": true
-      },
-      {
-          "name": "yoshida",
-          "at_name": "@yoshida",
-          "switch": true
-      },
-      {
-          "name": "tanaka",
-          "at_name": "@tanaka",
-          "switch": false
-      },
-      {
-          "name": "suzuki",
-          "at_name": "@suzuki",
-          "switch": true
-      },
-      {
-          "name": "yoshida",
-          "at_name": "@yoshida",
-          "switch": true
-      },
-      {
-          "name": "tanaka",
-          "at_name": "@tanaka",
-          "switch": false
-      },
-      {
-          "name": "suzuki",
-          "at_name": "@suzuki",
-          "switch": true
-      },
-      {
-          "name": "yoshida",
-          "at_name": "@yoshida",
-          "switch": true
-      },
-      {
-          "name": "tanaka",
-          "at_name": "@tanaka",
-          "switch": false
-      },
-      {
-          "name": "suzuki",
-          "at_name": "@suzuki",
-          "switch": true
-      },
-      {
-          "name": "yoshida",
-          "at_name": "@yoshida",
-          "switch": true
+  //シングルトンパターンの書き方
+  static let shared = API()
+  private init() { }
+
+  //userデータを取得するAPI
+  func getUsers(completion: (([MVCUserModel]) -> Void)? = nil) {
+    //仮想API通信
+    //本来はサーバーから取得する
+    //ここではサンプルとしてsampeDataを0.4sec後に受け取っている
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+
+      guard
+        let data = API.sampleData.data(using: .utf8),
+        //Codable(Decodable)に準拠したモデルはjson文字列から自動で変わる
+        let users = try? JSONDecoder().decode([MVCUserModel].self, from: data)
+        else {
+          completion?([])
+          return
       }
-  ]
-  """
-      return json
+      completion?(users)
     }
+  }
+
+  //サーバーからこんな感じでデータが返ってくるとする
+  private static var sampleData: String {
+    let json =
+"""
+[
+    {
+        "name": "tanaka",
+        "at_name": "@tanaka",
+        "switch": false
+    },
+    {
+        "name": "suzuki",
+        "at_name": "@suzuki",
+        "switch": true
+    },
+    {
+        "name": "yoshida",
+        "at_name": "@yoshida",
+        "switch": true
+    },
+    {
+        "name": "tanaka",
+        "at_name": "@tanaka",
+        "switch": false
+    },
+    {
+        "name": "suzuki",
+        "at_name": "@suzuki",
+        "switch": true
+    },
+    {
+        "name": "yoshida",
+        "at_name": "@yoshida",
+        "switch": true
+    },
+    {
+        "name": "tanaka",
+        "at_name": "@tanaka",
+        "switch": false
+    },
+    {
+        "name": "suzuki",
+        "at_name": "@suzuki",
+        "switch": true
+    },
+    {
+        "name": "yoshida",
+        "at_name": "@yoshida",
+        "switch": true
+    },
+    {
+        "name": "tanaka",
+        "at_name": "@tanaka",
+        "switch": false
+    },
+    {
+        "name": "suzuki",
+        "at_name": "@suzuki",
+        "switch": true
+    },
+    {
+        "name": "yoshida",
+        "at_name": "@yoshida",
+        "switch": true
+    },
+    {
+        "name": "tanaka",
+        "at_name": "@tanaka",
+        "switch": false
+    },
+    {
+        "name": "suzuki",
+        "at_name": "@suzuki",
+        "switch": true
+    },
+    {
+        "name": "yoshida",
+        "at_name": "@yoshida",
+        "switch": true
+    },
+    {
+        "name": "tanaka",
+        "at_name": "@tanaka",
+        "switch": false
+    },
+    {
+        "name": "suzuki",
+        "at_name": "@suzuki",
+        "switch": true
+    },
+    {
+        "name": "yoshida",
+        "at_name": "@yoshida",
+        "switch": true
+    },
+    {
+        "name": "tanaka",
+        "at_name": "@tanaka",
+        "switch": false
+    },
+    {
+        "name": "suzuki",
+        "at_name": "@suzuki",
+        "switch": true
+    },
+    {
+        "name": "yoshida",
+        "at_name": "@yoshida",
+        "switch": true
+    }
+]
+"""
+    return json
+  }
 }
