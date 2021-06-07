d9fcf9b6b926dd2c47bb8d43e529d02edf41bc16
Ohara <nori.tukareta@gmail.com>
2021-06-08 03:30:25 +0900

    sliderの値でimageの大きさを変更する処理実装

--- MoveImageSampleApp/MoveImageSampleApp/ViewController.swift
+++ MoveImageSampleApp/MoveImageSampleApp/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sampleSlider: UISlider!
+    
+    // uiimageの初期化
+    
+    
+    let imageview = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,14 +27,14 @@ class ViewController: UIViewController {
     func initImageView() {
         // uiimageのインスタンス作成
         let image: UIImage = UIImage(named: "image")!
-        // uiimageの初期化
-        let imageview = UIImageView(image: image)
-        
+        imageview.image = image
         // 画面のサイズを取得
         let screenWidth: CGFloat = view.frame.size.width
         let screenHeight: CGFloat = view.frame.size.height
         
         // MARK: - sample image
+        
+       
         // imageの大きさを指定
         var imageWidth: CGFloat = 100
         var imageHeight: CGFloat = 100
@@ -42,6 +49,28 @@ class ViewController: UIViewController {
         self.view.addSubview(imageview)
         
         
+        
+        
     }
+    
+    @IBAction func sampleSliderChange(_ sender: Any) {
+        // スライダーの値を取得
+        sampleSlider.maximumValue = 200
+        sampleSlider.minimumValue = 0
+        var changeSize = sampleSlider.value
+        // imageの大きさを指定
+//        var imageWidth: CGFloat = 100
+//        var imageHeight: CGFloat = 100
+        
+        var imageWidth: CGFloat = CGFloat(changeSize)
+        var imageHeight: CGFloat = CGFloat(changeSize)
+        
+        var rect: CGRect = CGRect(x: 100, y: 100, width: imageWidth, height: imageHeight)
+        
+        // 画像のサイズを合わせる
+        imageview.frame = rect
+        
+    }
+    
 }
 
