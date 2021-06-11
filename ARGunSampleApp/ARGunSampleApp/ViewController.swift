ddd6c992c10ae860b606d4b9d9e2c422424e293f
Ohara <nori.tukareta@gmail.com>
2021-06-11 23:24:59 +0900

    トリガーボタン設置

---
+++ ARGunSampleApp/ARGunSampleApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ARGunSampleApp
+//
+//  Created by 尾原徳泰 on 2021/06/11.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
