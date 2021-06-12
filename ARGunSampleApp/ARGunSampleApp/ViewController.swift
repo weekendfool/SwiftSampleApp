b58dd51b113f3e0256b003c404192bf98ae464b4
Ohara <nori.tukareta@gmail.com>
2021-06-12 23:33:28 +0900

    起動時の設定実装

--- ARGunSampleApp/ARGunSampleApp/ViewController.swift
+++ ARGunSampleApp/ARGunSampleApp/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    @IBAction func gunButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.delegate = self
+        
+        sceneView.scene = SCNScene()
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
         
     }
-    
+}
     
