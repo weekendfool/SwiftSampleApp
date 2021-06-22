e5dfa2297f69fa28c55767a3939d487ad3994033
Ohara <nori.tukareta@gmail.com>
2021-06-22 15:14:58 +0900

    ARViewに換装

---
+++ 3dBodySampleApp/3dBodySampleApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  3dBodySampleApp
+//
+//  Created by 尾原徳泰 on 2021/06/22.
+//
+
+import UIKit
+import Combine
+import ARKit
+import RealityKit
+
+class ViewController: UIViewController, ARSessionDelegate {
+
+   
+    @IBOutlet weak var sceneView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
