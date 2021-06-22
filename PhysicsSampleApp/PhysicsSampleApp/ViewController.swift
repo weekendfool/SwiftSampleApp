8f63d7523b9adba554806983e33efc2f6f59e43f
Ohara <nori.tukareta@gmail.com>
2021-06-22 16:01:22 +0900

    addSphere実装

---
+++ PhysicsSampleApp/PhysicsSampleApp/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  PhysicsSampleApp
+//
+//  Created by 尾原徳泰 on 2021/06/22.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // デリゲートの設定
+        sceneView.delegate = self
+        // シーンの作成と登録
+        sceneView.scene = SCNScene()
+        
+        // 特徴点を描画
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        // ライトの追加
+        sceneView.autoenablesDefaultLighting = true
+        
+        // 平面を検出
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+    }
+    
+    //球の追加
+    func addSphere(hitResult: ARHitTestResult) {
+        // ノードの作成
+        let spherelNode = SCNNode()
+        
+        // Geometryとtoransformの設定
+        let sphereGeometry = SCNSphere(radius: 0.03)
+        spherelNode.geometry = sphereGeometry
+        spherelNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y + 0.05, hitResult.worldTransform.columns.3.z)
+        
+        // ノードの追加
+        sceneView.scene.rootNode.addChildNode(spherelNode)
+    }
+}
