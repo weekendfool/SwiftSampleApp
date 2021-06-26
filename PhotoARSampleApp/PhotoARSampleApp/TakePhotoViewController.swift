bf7b3893a4110ca5316e843719eb9f1774f16ee6
Ohara <nori.tukareta@gmail.com>
2021-06-27 03:52:20 +0900

    階層分け

---
+++ PhotoARSampleApp/PhotoARSampleApp/TakePhotoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TakePhotoViewController.swift
+//  PhotoARSampleApp
+//
+//  Created by 尾原徳泰 on 2021/06/27.
+//
+
+import UIKit
+
+
+
+class TakePhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
