b27e8f2d69aa0ff332ce858e56f34f4e348b93f3
Ohara <nori.tukareta@gmail.com>
2021-06-29 16:04:02 +0900

    API取得サンプルアプリ

---
+++ ApiSampleApp/ApiSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiSampleApp
+//
+//  Created by 尾原徳泰 on 2021/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
