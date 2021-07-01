a17791e78fc8dfaa519a3ca5f46c244824aa3bf1
Ohara <nori.tukareta@gmail.com>
2021-07-01 14:04:20 +0900

    Booksファイル実装

---
+++ ApiSampleApp/ApiSampleApp/Model/Books.swift
@@ -0,0 +1,15 @@
+//
+//  API.swift
+//  ApiSampleApp
+//
+//  Created by 尾原徳泰 on 2021/07/01.
+//
+
+import Foundation
+import UIKit
+
+struct Books: Codable {
+    let name: String
+    let isbn: Int
+//    let image: UIImage
+}
---
+++ ApiSampleApp/ApiSampleApp/Model/GetAPI.swift
@@ -0,0 +1,8 @@
+//
+//  GetAPI.swift
+//  ApiSampleApp
+//
+//  Created by 尾原徳泰 on 2021/07/01.
+//
+
+import Foundation
