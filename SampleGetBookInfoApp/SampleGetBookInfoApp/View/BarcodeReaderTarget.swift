e915e95cf207926a9e62509a0eaa292e28956595
Ohara <nori.tukareta@gmail.com>
2021-07-03 23:14:19 +0900

    BarcodeReaderTarget実装

--- SampleGetBookInfoApp/SampleGetBookInfoApp/View/BarcodeReaderTarget.swift
+++ SampleGetBookInfoApp/SampleGetBookInfoApp/View/BarcodeReaderTarget.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+
+struct BarcodeReaderTarget {
+    
+    // ターゲット用の赤枠を生成する処理
+    func setUpTargetView(vc: UIViewController) {
+        let x: CGFloat = vc.view.frame.size.width * 0.1
+        let y: CGFloat = vc.view.frame.size.height * 0.4
+        let width: CGFloat = vc.view.frame.size.width * 0.8
+        let height: CGFloat = vc.view.frame.size.height * 0.2
+        
+        // 読み取り可能エリアに赤枠を追加
+        let barcodeReadAreaView = UIView()
+        barcodeReadAreaView.frame = CGRect(
+            x: x,
+            y: y,
+            width: width,
+            height: height
+        )
+        
+        barcodeReadAreaView.layer.backgroundColor = UIColor.red.cgColor
+        barcodeReadAreaView.layer.borderWidth = 4
+        vc.view.addSubview(barcodeReadAreaView)
+    }
+}
