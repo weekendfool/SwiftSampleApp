a719f30a66581f4f01316025f87a267130856db2
Ohara <nori.tukareta@gmail.com>
2021-07-09 21:28:11 +0900

    コードでNavigationController実装

---
+++ MakeNavigationControllerSampleApp/MakeNavigationControllerSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeNavigationControllerSampleApp
+//
+//  Created by Oh!ara on 2021/07/09.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
